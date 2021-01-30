b279b901d0b0cfc9caa78ccd1d4684f910558523
Anastasia Shpit <itshpit@gmail.com>
2021-01-31 00:05:45 +0200

    Add Hooks to project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockateil" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams/>
     </div>
-  )
+  );
 };
 
 //this is going to stamp our App
