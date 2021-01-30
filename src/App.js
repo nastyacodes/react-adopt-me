5905e2a89725df3fdcd910b4afdac57d29bf4312
Anastasia Shpit <itshpit@gmail.com>
2021-01-30 23:52:58 +0200

    Convert React component to JSX

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import React from 'react';
+import React from "react";
 import { render } from "react-dom";
-import Pet from './Pet';
+import Pet from "./Pet";
 
 const App = () => {
-  // created a stamp [COMPONENT]
-  return React.createElement(
-    "div", //what kind of tag
-    { id: "something-important" }, //attributes we give to component
-    [
-      React.createElement("h1", {}, "Adopt Me!"), //children of this element
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }), // if we don't put {} and [] it means it has no attributes and children
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockateil",
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "Cat",
-        breed: "Mixed",
-      }),
-    ] // it can also be an array
-  );
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockateil" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  )
 };
 
-render(
-  //this is going to stamp our App
-  React.createElement(App),
-  document.getElementById("root")
-);
+//this is going to stamp our App
+render(<App/>, document.getElementById("root"));
