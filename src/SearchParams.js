98c0ca997bb6f7e626804b3e2e2f3337e86c4de2
Anastasia Shpit <itshpit@gmail.com>
2021-01-31 23:38:35 +0200

    Apply Hooks for SearchParams

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchParams = () => {
-    const location = "Seatle, WA";
+    const [ location, setLocation ] = useState("Seatle, WA"); //it always returnes an ARRAY
 
     return (
         <div className="search-params">
+            <h1>{location}</h1>
             <form>
                 <label htmlFor="location">
                     Location
-                    <input id="location" value={location} placeholder="Location"/>
+                    <input 
+                        id="location" 
+                        value={location} 
+                        placeholder="Location" 
+                        onChange={e => setLocation(e.target.value)} //any time this event working the input value changes
+                    />
                 </label>
                 <button>Submit</button>
             </form>
